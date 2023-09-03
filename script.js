2141add6b35d1eb1083f7f1d1effacdd81c5c9d3
nick0la85 <nicdimarco@rocketmail.com>
2023-09-03 20:00:57 +0200

    ok

--- script.js
+++ script.js
@@ -1,87 +1,189 @@
-function updatePartecipantiTable(data) {
-    const partecipantiTable = document.getElementById("partecipanti-table");
-    const tbody = partecipantiTable.querySelector("tbody");
-    tbody.innerHTML = ""; // Pulisce il contenuto della tabella
+function updatePartecipantiCards(data) {
+  const partecipantiContainer = document.getElementById(
+    "partecipanti-container"
+  );
+  partecipantiContainer.innerHTML = ""; // Pulisce il contenuto del contenitore
 
-    data.forEach((item, index) => {
-        const row = tbody.insertRow();
-        const cell1 = row.insertCell(0);
-        const cell2 = row.insertCell(1);
-        const cell3 = row.insertCell(2);
+  const row = document.createElement("div");
+  row.className = "row";
 
-        cell1.textContent = index + 1; // Numero di partecipante
-        cell2.textContent = item.nome; // Nome del partecipante
-        cell3.textContent = item.quota; // Quota del partecipante
-    });
+  data.forEach((item) => {
+    const card = document.createElement("div");
+    card.className = "card custom-card";
+
+    const col = document.createElement("div");
+    col.className = "col-md-4";
+
+    const cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+
+    const icon = document.createElement("i");
+    icon.className = item.iconClass; // Aggiungi la classe iconClass specificata nell'array
+    cardBody.appendChild(icon);
+
+    const nome = document.createElement("h5");
+    nome.className = "card-title";
+    nome.textContent = item.nome; // Nome del partecipante
+    cardBody.appendChild(nome);
+
+    const quota = document.createElement("p");
+    quota.className = "card-text";
+    quota.innerHTML = `<i class="fas fa-euro-sign"></i> ${item.quota}`; // Icona dell'euro prima della quota
+    cardBody.appendChild(quota);
+
+    card.appendChild(cardBody);
+    partecipantiContainer.appendChild(card);
+  });
 }
 
 // Funzione per aggiornare la tabella delle spese
-function updateSpeseTable(data) {
-    const speseTable = document.getElementById("spese-table");
-    const tbody = speseTable.querySelector("tbody");
-    tbody.innerHTML = ""; // Pulisce il contenuto della tabella
+function updateSpeseCards(data) {
+  const speseContainer = document.getElementById("spese-container");
+  speseContainer.innerHTML = ""; // Pulisce il contenuto del contenitore
 
-    data.forEach((item, index) => {
-        const row = tbody.insertRow();
-        const cell1 = row.insertCell(0);
-        const cell2 = row.insertCell(1);
-        const cell3 = row.insertCell(2);
+  const row = document.createElement("div");
+  row.className = "row";
 
-        cell1.textContent = index + 1; // Numero di spesa
-        cell2.textContent = item.nome; // Nome della spesa
-        cell3.textContent = item.quota; // Quota della spesa
-    });
+  data.forEach((item) => {
+    const card = document.createElement("div");
+    card.className = "card custom-card";
+
+    const col = document.createElement("div");
+    col.className = "col-md-4";
+
+    const cardBody = document.createElement("div");
+    cardBody.className = "card-body";
+
+    const icon = document.createElement("i");
+    icon.className = item.iconClass; // Aggiungi la classe iconClass specificata nell'array
+    cardBody.appendChild(icon);
+
+    const descrizione = document.createElement("h5");
+    descrizione.className = "card-title";
+    descrizione.textContent = item.nome; // Descrizione della spesa
+    cardBody.appendChild(descrizione);
+
+    const quota = document.createElement("p");
+    quota.className = "card-text";
+    quota.innerHTML = `<i class="fas fa-euro-sign"></i> ${item.quota}`; // Icona dell'euro prima della quota
+    cardBody.appendChild(quota);
+
+    card.appendChild(cardBody);
+    speseContainer.appendChild(card);
+  });
 }
 
 // Esempio dati per le tabelle (sostituisci con i tuoi dati reali)
 const partecipantiData = [
-    { nome: "Antonio", quota:0},
-    { nome: "Pino", quota:0},
-    { nome: "Giuseppe", quota:0},
-    { nome: "Filippo", quota:0},
-    { nome: "Nicola", quota:10},
-    { nome: "Franco", quota:0},
-    { nome: "Paolo", quota:0},
-    { nome: "Aldo", quota:0},
-    { nome: "Mimmo", quota:0},
-   
+  {
+    nome: "Antonio",
+    iconClass: "fa-sharp fa-solid fa-person-biking fa-beat",
+    iconaEuro: "€",
+    quota: 50 + 20,
+  },
+  {
+    nome: "Pino",
+    iconClass: "fa-sharp fa-solid fa-person-biking fa-beat",
+    iconaEuro: "€",
+    quota: 0,
+  },
+  {
+    nome: "Giuseppe",
+    iconClass: "fa-sharp fa-solid fa-person-biking fa-beat",
+    iconaEuro: "€",
+    quota: 0,
+  },
+  {
+    nome: "Filippo",
+    iconClass: "fa-sharp fa-solid fa-person-biking fa-beat",
+    iconaEuro: "€",
+    quota: 0,
+  },
+  {
+    nome: "Nicola",
+    iconClass: "fa-sharp fa-solid fa-person-biking fa-beat",
+    iconaEuro: "€",
+    quota: 0,
+  },
+  {
+    nome: "Franco",
+    iconClass: "fa-sharp fa-solid fa-person-biking fa-beat",
+    iconaEuro: "€",
+    quota: 0,
+  },
+  {
+    nome: "Paolo",
+    iconClass: "fa-sharp fa-solid fa-person-biking fa-beat",
+    iconaEuro: "€",
+    quota: 0,
+  },
+  {
+    nome: "Aldo",
+    iconClass: "fa-sharp fa-solid fa-person-biking fa-beat",
+    iconaEuro: "€",
+    quota: 0,
+  },
+  {
+    nome: "Mimmo",
+    iconClass: "fa-sharp fa-solid fa-person-biking fa-beat",
+    iconaEuro: "€",
+    quota: 0,
+  },
 ];
 
 const speseData = [
-    { nome: "Albergo", quota:0},
-    { nome: "Ristorante", quota:0},
-    { nome:"Bar", quota: 5},
-    { nome:"Benzina", quota: 0},
-    { nome:"Varie", quota: 0}
+  { nome: "Albergo", iconClass: "fas fa-hotel", iconaEuro: "€", quota: 20 },
+  {
+    nome: "Ristorante",
+    iconClass: "fas fa-utensils",
+    iconaEuro: "€",
+    quota: 0,
+  },
+  { nome: "Bar", iconClass: "fas fa-coffee", iconaEuro: "€", quota: 0 },
+  { nome: "Benzina", iconClass: "fas fa-gas-pump", iconaEuro: "€", quota: 0 },
+  { nome: "Varie", iconClass: "fas fa-shopping-bag", iconaEuro: "€", quota: 0 },
+  // Aggiungi altre spese qui...
 ];
 
 // Aggiorna le tabelle con i dati
-updatePartecipantiTable(partecipantiData);
-updateSpeseTable(speseData);
+updatePartecipantiCards(partecipantiData);
+updateSpeseCards(speseData);
 
 function calcolaTotale() {
-    const partecipantiTable = document.getElementById("partecipanti-table");
-    const speseTable = document.getElementById("spese-table");
+  const partecipantiContainer = document.getElementById(
+    "partecipanti-container"
+  );
+  const speseContainer = document.getElementById("spese-container");
 
-    const partecipantiRows = partecipantiTable.querySelectorAll("tbody tr");
-    const speseRows = speseTable.querySelectorAll("tbody tr");
+  const partecipantiCards = partecipantiContainer.querySelectorAll(".card");
+  const speseCards = speseContainer.querySelectorAll(".card");
 
-    let totaleQuotePartecipanti = 0;
-    let totaleSpese = 0;
+  let totaleQuotePartecipanti = 0;
+  let totaleSpese = 0;
 
-    partecipantiRows.forEach((row) => {
-        const quota = parseFloat(row.cells[2].textContent.replace("€", "").trim());
-        totaleQuotePartecipanti += quota;
-    });
+  partecipantiCards.forEach((card) => {
+    const quota = parseFloat(
+      card
+        .querySelector(".card-text")
+        .textContent.replace("Quota: €", "")
+        .trim()
+    );
+    totaleQuotePartecipanti += quota;
+  });
 
-    speseRows.forEach((row) => {
-        const quota = parseFloat(row.cells[2].textContent.replace("€", "").trim());
-        totaleSpese += quota;
-    });
+  speseCards.forEach((card) => {
+    const quota = parseFloat(
+      card
+        .querySelector(".card-text")
+        .textContent.replace("Quota: €", "")
+        .trim()
+    );
+    totaleSpese += quota;
+  });
 
-    const totaleDisponibile = totaleQuotePartecipanti - totaleSpese;
-    const totalMoneyElement = document.getElementById("total-money");
-    totalMoneyElement.textContent = `€ ${totaleDisponibile.toFixed(2)}`;
+  const totaleDisponibile = totaleQuotePartecipanti - totaleSpese;
+  const totalMoneyElement = document.getElementById("total-money");
+  totalMoneyElement.textContent = `€ ${totaleDisponibile.toFixed(2)}`;
 }
 
 // Chiama la funzione al caricamento della pagina per inizializzare il totale
